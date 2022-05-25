9221b2bce6fe8d1e8a6807f10ba8c6a2bc03dd13
EddyPrime <edoardo.liberati27@gmail.com>
2022-05-25 17:08:50 +0200

    Login function

---
+++ functions/functions.php
@@ -0,0 +1,21 @@
+<?php
+
+function redirect_to_homepage() {
+    header("location:../home/home.php");
+}
+
+function redirect_to_login() {
+    header("location:../login/login.php");
+}
+
+function isLoggedIn() {
+    session_start();
+    if (!$_SESSION["loggedIn"]) {
+        redirect_to_login();
+        die;
+    }
+    else {
+        redirect_to_homepage();
+    }
+}
+?>
--- index.php
+++ index.php
@@ -1,16 +1,25 @@
 <!doctype html>
 <html>
-    <head>
-       	<meta charset="utf-8">
-       	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, shrink-to-fit=no">
-        <title>Groomy</title>
 
-        <link rel="stylesheet" type="text/css" href="./style/reset.css">
-        <link rel="stylesheet" type="text/css" href="./style/fonts.css">
-        <link rel="stylesheet" type="text/css" href="./style/style.css">
-		<link rel="stylesheet" type="text/css" href="./dist/css/bootstrap.min.css">
-    </head>
-    <body>
-		Groomy!
-    </body>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, shrink-to-fit=no">
+    <title>Groomy</title>
+
+    <link rel="stylesheet" type="text/css" href="./style/reset.css">
+    <link rel="stylesheet" type="text/css" href="./style/fonts.css">
+    <link rel="stylesheet" type="text/css" href="./style/style.css">
+    <link rel="stylesheet" type="text/css" href="./dist/css/bootstrap.min.css">
+
+    <?php
+    include "./functions/functions.php";
+    ?>
+</head>
+
+<body>
+    <?php
+    isLoggedIn();
+    ?>
+</body>
+
 </html>
