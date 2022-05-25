d39825fe0414633b8ebadbeae7874adb9b41d8d2
EddyPrime <edoardo.liberati27@gmail.com>
2022-05-25 16:36:28 +0200

    Initial structure for website

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+    <head>
+       	<meta charset="utf-8">
+       	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, shrink-to-fit=no">
+        <title>Groomy</title>
+
+        <link rel="stylesheet" type="text/css" href="./style/reset.css">
+        <link rel="stylesheet" type="text/css" href="./style/fonts.css">
+        <link rel="stylesheet" type="text/css" href="./style/style.css">
+		<link rel="stylesheet" type="text/css" href="./dist/css/bootstrap.min.css">
+    </head>
+    <body>
+		Groomy!
+    </body>
+</html>
