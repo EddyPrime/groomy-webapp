8971c7b22722bf6c26ff43817049e07fd9ff7006
Giordano Laurenzi <giordanolaurenzi@MacBook-Pro-di-Giordano.local>
2022-05-30 12:27:15 +0200

    functions login

--- functions/functions.php
+++ functions/functions.php
@@ -10,3 +10,18 @@ function isLoggedIn()
     session_start();
     return $_SESSION["loggedIn"];
 }
+
+function existingAccount($filename, $account)
+{
+    $users = explode("\n", file_get_contents($filename));
+    $res = [];
+    for ($i = 0; $i < count($users); $i++) {
+        $infoElementArray = explode(";", $users[$i]);
+        for ($j = 0; $j < count($infoElementArray); $j++) {
+            if ($infoElementArray[0] . $infoElementArray[1] == $account) {
+                array_push($res, $infoElementArray[0], $infoElementArray[1], $infoElementArray[2], $infoElementArray[3], $infoElementArray[4]);
+                return $res;
+            }
+        }
+    }
+}
