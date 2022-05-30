0bb97c7b5c45bd80abc87212a89809b105649d0a
Giordano Laurenzi <giordanolaurenzi@MacBook-Pro-di-Giordano.local>
2022-05-30 12:28:17 +0200

    home

--- home/home.php
+++ home/home.php
@@ -1,1 +1,23 @@
 This is the homepage.
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, shrink-to-fit=no">
+    <title>Home</title>
+
+    <link rel="stylesheet" type="text/css" href="../style/reset.css">
+    <link rel="stylesheet" type="text/css" href="../style/fonts.css">
+    <link rel="stylesheet" type="text/css" href="../style/style.css">
+    <link rel="stylesheet" type="text/css" href="../dist/css/bootstrap.min.css">
+
+    <?php
+    include "../navbar/navbar.php";
+    ?>
+</head>
+
+<body>
+    
+
+</html>
