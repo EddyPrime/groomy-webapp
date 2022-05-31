8eaa5617de026a70defc5f14cb9de50cc9c1f247
EddyPrime <edoardo.liberati27@gmail.com>
2022-05-31 11:22:03 +0200

    navbar

--- navbar/navbar.php
+++ navbar/navbar.php
@@ -5,20 +5,20 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
           <i class="material-icons">home</i>
+          <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="../booking/booking.php">Booking</a>
           <i class="material-icons">bookmarks</i>
+          <a class="nav-link" href="../booking/booking.php">Booking</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="../pets/pets.php">Pets</a>
           <i class="material-icons">pets</i>
+          <a class="nav-link" href="../pets/pets.php">Pets</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="../profile/profile.php">Profile</a>
           <i class="material-icons">person</i>
+          <a class="nav-link" href="../profile/profile.php">Profile</a>
         </li>
       </ul>
 
