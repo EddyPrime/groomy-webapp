4efd11be6ebcc4d56d9e95ae8828f5285154a95a
MattLomb <matlomb97@gmail.com>
2022-06-29 20:36:45 +0200

    Fix delete button in appointment list

--- home/home.php
+++ home/home.php
@@ -11,6 +11,10 @@
     <link rel="stylesheet" type="text/css" href="../style/style.css">
     <link rel="stylesheet" type="text/css" href="../dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
     <?php
     include "../navbar/navbar.php";
@@ -71,16 +75,17 @@
                         $class = 'denied';
                     }
 
-                    if ($date < date('d-m-y')) {
+                    if ($date < '20' . date('y-m-d')) {
                         $class = 'passed';
                     }
 
                     echo '<li>
-                            <a href="../appointment/appointment.php?groomer=' . $groomer . '&pet_name=' . $pet . '&date=' . $date . '"> 
-                                <div class="appointment-info ' . $class . '" >
+                            <div class="appointment-info ' . $class . '" >
+                                <a href="../appointment/appointment.php?groomer=' . $groomer . '&pet_name=' . $pet . '&date=' . $date . '"> 
                                     Groomer: <span id="name">' . $groomer . '</span><br> Address: ' . $address . '<br> Pet: ' . $pet . '<br> Date: ' . $date . '<br>Hour: ' . $hour . '  
-                                </div>
-                            </a>
+                                </a>
+                                <button class="btn-delete-appointment btn btn-lg btn-primary btn-block mb-4 btn-danger" style="margin-top: 5%;" type="button" onclick="deleteappointment(this)" name="delete_appointment" value="'. $groomer . ';' . $pet . ';' . $date . '"><i class="fa-solid fa-trash-can"></i></button> 
+                            </div>
                         </li>';
 
                     $printed++;
@@ -102,3 +107,38 @@
 </body>
 
 </html>
+
+<script>
+function deleteappointment(obj) {
+    value = obj.value
+    info = value.split(";");
+    parameter1 = info[0];
+    parameter2 = info[1];
+    parameter3 = info[2];
+    //alert(info[1]);
+    if (confirm("Are you sure you want to delete your appointment?")) {
+        var deleteAppointment = function() {
+            $.ajax({
+                url: '../appointment/delete_appointment.php',
+                type: 'GET',
+                data: {
+                    groomer: parameter1,
+                    pet_name: parameter2,
+                    date: parameter3
+                },
+                success: function(data) {
+                    //window.location.reload();
+                    console.log(data); // Inspect this in your console
+                },
+                complete: function(){
+                    window.location.reload();
+                }
+            });
+        };
+        deleteAppointment();
+        //setTimeout( window.location.reload(), 1000 );
+    }
+}
+
+
+</script>
